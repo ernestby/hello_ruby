2675f1e97a2d361ecf2202a023e3c1e9ca3e290c
Ernestby <your.ernestby@gmail.com>
2016-09-27 23:31:12 +0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FisrtProject::Application.config.secret_key_base = '99d73ab1185a7341d7908fe6e2d6fe44ba7a0da23d6b9f9220c617e8fa9760df47419f43c4d0851e572fb9e6149a4cbcd97ae8b7577b875564dfc82be1735412'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FisrtProject::Application.config.session_store :cookie_store, key: '_fisrt_project_session'
